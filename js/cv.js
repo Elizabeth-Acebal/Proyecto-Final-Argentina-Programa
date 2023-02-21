3d7890a55733bd5d3786d32fb838ca0d5a253cb3
Elizabeth-Acebal <elyacebal26@gmail.com>
2023-02-21 14:41:49 -0300

    añadi archivos

---
+++ js/cv.js
@@ -0,0 +1,9 @@
+"use strict";
+let btn = document.querySelector("#btn");
+btn.addEventListener("click" , abrirParrafo);
+
+
+function abrirParrafo(){
+    document.querySelector("#parrafo").innerHTML= "Para ver mas sobre mis proyectos, mirá mi github"; 
+                                           
+;}                                                 
